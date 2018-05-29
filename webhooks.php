4481c7743f45d73192d127f8733ecf9bff466f43
looby10120 <mosfb@hotmail.com>
2018-05-29 19:41:14 +0700

    Change access token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'fefU9HSA3alGd18OR0+B5ntb/1CIwaXTaLIwlIlm9D0ql76SZOAVdYCzTsfUx65rAp12Dc3K98Hl0yXsR1BK6C8VfZTVrKprNMfcSaeuwtEkTnY50nAau6A9P0+mqneNZIZV9jr5bkWQPZQmFP63VwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
